45750a66ba5bd65bc306141abd73696a598b7bbd
mishodzuliashvili <mishodzuliashvili1@gmail.com>
2024-02-16 13:42:26 +0400

    Make improvements

--- next.config.mjs
+++ next.config.mjs
@@ -6,27 +6,7 @@ const nextConfig = {
       bodySizeLimit: "100mb",
     }
   },
-  // reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/generate',
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+
   images: {
     remotePatterns: [
       {
--- src/app/api/trans/route.ts
+++ src/app/api/trans/route.ts
@@ -4,8 +4,8 @@ export async function GET(request: Request) {
     try {
         const { searchParams } = new URL(request.url)
         const id = searchParams.get('id')
-        // 'kO1kgl0p-Hw'
         const rtes = await YoutubeTranscript.fetchTranscript(id!);
+        console.log(rtes)
         const m = rtes.map((rte) => rte.text).join(" ")
         return Response.json({
             result: m
